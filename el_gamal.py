96cef2c8c058cb124615120f3f87539ff268c9c8
Berkay Kaya <98612716+khesly1903@users.noreply.github.com>
2024-09-26 01:17:50 +0300

    Update el_gamal.py
    el gamal class

--- el_gamal.py
+++ el_gamal.py
@@ -3,66 +3,40 @@ import random
 from typing import Tuple
 
 
+class ElGamal:
+    def __init__(self, curve):
+        self.curve = curve
+        self.P = curve.GPoint
 
-#Public parameter
-curve = ec.P256
-P = ec.P256.GPoint
+    def key(self, n: int) -> ec.Point:
+        return ec.s_multiplication(self.curve, self.P, n)
 
-
-def key(n:int) -> ec.Point:
-    return ec.s_multiplication(curve, P, n)
-
-
-def encryption(keyQ:ec.Point, msg:str) -> Tuple[list, list]:
-    msg_points = ec.msg_to_points(curve,msg)
-    
-    shifts = []
-    for point in msg_points:
-        shifts.append(point.get_shift)
-    
-    k = random.randint(1, curve.p -1)
-    c_1 = k * P
-    
-    encrypted = []
-    for i in range(len(msg_points)):
-        c_2 = msg_points[i] + (k * keyQ)
-        encrypted.append((c_1,c_2))
+    def encryption(self, keyQ: ec.Point, msg: str) -> Tuple[list, list]:
+        msg_points = ec.msg_to_points(self.curve, msg)
         
-    return encrypted, shifts 
+        shifts = []
+        for point in msg_points:
+            shifts.append(point.get_shift)
+        
+        k = random.randint(1, self.curve.p - 1)
+        c_1 = k * self.P
+        
+        encrypted = []
+        for i in range(len(msg_points)):
+            c_2 = msg_points[i] + (k * keyQ)
+            encrypted.append((c_1, c_2))  
+            
+        return shifts, encrypted 
 
-
-def decryption(n:int, shifts:list, enc:list) -> str:
-    
-    decrypted = []
-    for e in range(len(enc)):
-        c_1, c_2 = enc[e]
-        m = c_2 - (n * c_1)
-        m = m.set_shift(shifts[e])
-
-        decrypted.append(m)
-          
-    dec_str = ec.point_to_msg(decrypted)
-
-    return dec_str
-
-
-
-
-msg = "Ataturk came to prominence for his role in securing the Ottoman Turkish victory at the Battle of Gallipoli (1915) during World War I.[6] During this time, the Ottoman Empire perpetrated genocides against its Greek, Armenian and Assyrian subjects; while never involved, Ataturk's role in their aftermath was the subject of discussion. Following the defeat of the Ottoman Empire after World War I, he led the Turkish National Movement, which resisted mainland Turkey's partition among the victorious Allied powers."
-
-
-keyQ = key(123)
-
-enc, shifts = encryption(keyQ,msg)
-
-
-print(f"\n\nENCRYPTED\n{enc}\nshifts: {shifts}")
+    def decryption(self, n: int, shifts: list, enc: list) -> str:
+        decrypted = []
+        for e in range(len(enc)):
+            c_1, c_2 = enc[e]
+            m = c_2 - (n * c_1)
+            m = m.set_shift(shifts[e])
+            decrypted.append(m)
+        
+        return ec.point_to_msg(decrypted)
 
 
 
-dec = decryption(123,shifts,enc)
-print(f"\n\nDECRYPTED\n{dec}")
-
-
-if msg == dec:
-    print("\n\nmessages match")
